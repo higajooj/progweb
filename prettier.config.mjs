8e8b6a9920d81e8c3fabea45c3b945da54276e79
Daniel Higa <danielhiga10@gmail.com>
2025-04-06 18:32:35 -0400

    Initial commit.

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+  overrides: [],
+  // plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-css-order'],
+}
+
+export default config
